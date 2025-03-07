37ffc741b8f7d74a6753e4498e9469820af0c96a
Mark-Anthony25 <98404531+Mark-Anthony25@users.noreply.github.com>
2025-03-07 15:06:41 +0800

    added the hello world program

---
+++ HelloWorld/src/helloWorld/helloWorld.java
@@ -0,0 +1,43 @@
+package helloWorld;
+
+import java.util.Scanner;
+
+public class helloWorld {
+	static Scanner sc = new Scanner(System.in);
+	public static void main (String args[]) {
+		helloWorld hw = new helloWorld();
+
+		String namee = hello();
+		
+		greet(namee);
+
+		
+	}
+
+	public helloWorld() {
+		System.out.println("hello World this is an Object Oriented Programming");
+
+
+	}
+
+
+	private static void greet(String namee) {
+		int age = 0;
+		System.out.println("Enter your age: ");
+		age = sc.nextInt();
+		System.out.println("Hello" + namee + " Your age is: " + age);
+	}
+
+
+	private static String hello() {
+
+		System.out.println("Enter the name: ");
+		String name = sc.nextLine();
+		String greet = "Hello World";
+		for (int i = 1; 5 >= i; i++) {
+			System.out.println(greet + " " + name);
+		}
+		return name;
+	}
+
+}
